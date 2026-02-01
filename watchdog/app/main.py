f95c746daa3e8a400deacde325b970d9f1820389
Thales Nogueira <63682245+Thalessns@users.noreply.github.com>
2026-02-01 11:00:01 -0300

    Adding relatorio endpoints and service (#3)
    * Adding relatorio endpoints and service
    
    * Fixing some mistakes
    
    * Version bump

--- watchdog/app/main.py
+++ watchdog/app/main.py
@@ -5,6 +5,7 @@ from watchdog.database.database import Database
 from watchdog.routing.clientes.router import clientes_router
 from watchdog.routing.transacoes.router import transacoes_router
 from watchdog.routing.alertas.router import alertas_router
+from watchdog.routing.relatorios.router import relatorio_router
 
 
 @asynccontextmanager
@@ -14,7 +15,7 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(title="UBS Watchdog - Python", version="0.1.0", lifespan=lifespan)
+app = FastAPI(title="UBS Watchdog - Python", version="0.2.0", lifespan=lifespan)
 
 
 @app.get("/", status_code=status.HTTP_200_OK, response_model=dict[str, str])
@@ -26,3 +27,4 @@ prefix = "/api"
 app.include_router(clientes_router, prefix=prefix)
 app.include_router(transacoes_router, prefix=prefix)
 app.include_router(alertas_router, prefix=prefix)
+app.include_router(relatorio_router, prefix=prefix)
